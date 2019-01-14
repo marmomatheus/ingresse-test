8ec751c8a723f3c1700339cdd94a57df088e2b8b
Matheus <matheusmarmo55@gmail.com>
2019-01-14 20:03:19 -0200

    User factory

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -13,12 +13,13 @@ use Faker\Generator as Faker;
 |
 */
 
+$faker = \Faker\Factory::create('pt_BR');
+
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        "name" => $faker->name,
+        "email" => $faker->unique()->safeEmail,        
+        "cnpj" => $faker->cpf,
+        "password" => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm'  
     ];
 });
